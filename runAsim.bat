1b97b8951cfbb8f917c82be11d618d7e5b4ad8ce
Andrew Rohne <andrew.rohne@rsginc.com>
2021-09-17 06:32:20 -0400

    Updated mode choice

--- runAsim.bat
+++ runAsim.bat
@@ -1,3 +1,8 @@
 SET MKL_NUM_THREADS=1
+
+REM Run Cube Skimming, externals, freight, etc (TODO)
+
+REM Run Activitysim
 python simulation.py -c configs_mp -c configs -d data -o output_1003
 
+REM Run Cube Assignment (TODO)
