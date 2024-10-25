4f29df7c6068301b256c62a8695ab753f8246bd1
Andrew Rohne <andrew@siliconcreek.net>
2024-10-25 07:52:24 -0700

    Remove unnecessary dependencies

--- source/python/metc_nearestml.py
+++ source/python/metc_nearestml.py
@@ -1,8 +1,4 @@
 import pandana as pdna
-from IPython.display import display
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import numpy as np
 import geopandas as gpd
